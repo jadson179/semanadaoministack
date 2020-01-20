cc1a052d3a4a7d65741d9a0f39b70194bdca5b01
jadson179 <jadson44.santos@gmail.com>
2020-01-19 21:22:25 -0300

    add code application

---
+++ omnistack10/mobile/src/services/socket.js
@@ -0,0 +1,29 @@
+import socketio from 'socket.io-client'
+
+const socket = socketio('http://192.168.0.21:3333', {
+    autoConnect: false,
+})
+
+function subscribeToNewDevs(subscribeFunction) {
+    socket.on('new-dev', subscribeFunction)
+}
+
+function connect(latitude, longitude, techs){
+    socket.io.opts.query = {
+        latitude, longitude, techs
+    }
+    socket.connect()
+    
+    socket.on('message', text=> {
+        console.log(text)
+    })
+}
+function disconnect(){
+    socket.disconnect
+}
+
+export {
+    connect,
+    disconnect,
+    subscribeToNewDevs
+}
